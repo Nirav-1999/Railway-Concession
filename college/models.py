adf255b01ce38a2257f886e65bba3994b83ab0d4
Yash Soni <yashsoni77779@yahoo.com>
2019-02-09 17:02:01 +0530

    Student details model

--- college/models.py
+++ college/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class StudentData(models.Model):
+    student_name = models.CharField(max_length = 100)
+    student_bdate = models.DateField()
+    student_gender = models.CharField(max_length = 10)
+    student_add1 = models.CharField(max_length = 100)
+    student_add2 = models.CharField(max_length = 100)
+    student_aadhar = models.IntegerField()
+    student_station = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.student_name
+    
